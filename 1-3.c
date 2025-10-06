5a6729d5ff9e7df794b7019a330e117182e14c6e
Артём <AntiTor5708@yandex.ru>
2025-10-06 14:09:33 +0300

    Update 1-3.c

--- 1-3.c
+++ 1-3.c
@@ -1,7 +1,15 @@
-#define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdio.h>
+#include <locale.h>
 
+/**
+ * @brief рассчитывает сопротивление соединения
+ * @param R1 сопротивление резистора 1
+ * @param R2 сопротивление резистора 2
+ * @param R3 сопротивление резистора 3
+ * @return возвращает сопротивление соединения
+ */
+double getResistance(double R1, double R2, double R3);
 
 /**
  * @brief Точка входа в программу
@@ -9,19 +17,25 @@
  */
 int main(void)
 {
+    setlocale(LC_ALL, "Russian");
+
     double R1;
     double R2;
     double R3;
 
-    printf("Value 1: ");
+    printf("Введите сопротивление резистора 1: ");
     scanf_s("%lf", &R1);
-    printf("Value 2: ");
+    printf("Введите сопротивление резистора 2: ");
     scanf_s("%lf", &R2);
-    printf("Value 3: ");
+    printf("Введите сопротивление резистора 3: ");
     scanf_s("%lf", &R3);
 
-    printf("Result: ");
-    printf("V = %.2lf\n", (R1*R2*R3)/(R1*R2 + R2*R3 + R1*R3));
+    printf("Сопротивление соединения равно %.2lf\n", getResistance(R1, R2, R3));
 
     return 0;
+}
+
+double getResistance(double R1, double R2, double R3)
+{
+    return (R1 * R2 * R3) / (R1 * R2 + R2 * R3 + R1 * R3);
 }
